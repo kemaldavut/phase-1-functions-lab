e8f022952507816d1d649072e8e24cd2112f8385
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-04-21 16:09:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(a) {
+    return Math.abs(a - 42);
+}
+
+
+function distanceFromHqInFeet(a) {
+    return (distanceFromHqInBlocks(a) * 264 );
+}
+
+function distanceTravelledInFeet(a, b) {
+    return Math.abs(b - a) * 264;
+}
+
+
+function calculatesFarePrice(start, destination) {
+let total = distanceTravelledInFeet(start, destination);
+
+if (total < 400) {
+    return 0;
+}
+else if  (total > 400 && total < 2000) {
+    total = total - 400 
+    return total * 0.02;
+}
+
+else if  (total > 2000 && total < 2500) {
+        return 25;
+}
+
+else if  (total > 2500) {
+        return "cannot travel that far"
+}
+
+}
